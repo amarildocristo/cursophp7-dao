2b5266ec61a104231123e27b96c5bcf88689dbfb
Amarildo Cristo <amarildo_cristo85@hotmail.com>
2018-10-02 23:30:38 -0300

    iniciamos o prjeto

---
+++ class/sql.php
@@ -0,0 +1,57 @@
+<?php 
+	//Ao inves do php conversar com banco diretamente, ela passará por classes que vão auxiliar a coversa, criadas atraves do DAO(data access object). Essa é uma das grande vantagem do DAO/outa vantagem é a segurança.
+	class Sql extends PDO {
+
+		private $conn;
+
+		//Essa função é chamada automaticamente quando a classe for instanciada. Então a conexão com banco de dados é estabelecida no exato momento.
+		public function __construct(){
+
+			$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+		}
+
+		private function SetParams($statment, $parameters){
+
+			//Associando os parametros/dados
+			foreach ($parameters as $Chave => $valor) {
+
+				$this->setParam($Chave, $valor);
+			}			
+
+		}
+
+		private function SetParam($statment, $chave, $valor){
+
+			$statment->bindParam($chave, $valor);	
+
+		}
+
+		//Essa função serve para executar os comandos no banco.
+		//$rawQuery- query bruta!
+		public function query($rawQuery, $params = array()){
+
+			$stmt = $this->conn->prepare($rawQuery);
+
+			$this->setParams($stmt, $params);
+
+			$stmt->execute();
+
+			return $stmt;
+	
+		}
+
+		public function select($rawQuery, $params = array()):array
+		{
+
+			$stmt = $this->query($rawQuery, $params);
+			
+			//Traz apenas dados associativos sem os indexs
+			$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+			return $results;
+			
+		}
+
+	}
+
+ ?>
